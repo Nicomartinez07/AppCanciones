0b38bbb2a9fd6c305e306c05f89175cebb331741
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2024-05-06 08:50:16 -0300

    No se habia guardado todo

--- discos/__init__.py
+++ discos/__init__.py
@@ -1,8 +1,19 @@
-from flask import Flask
+import os 
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
+#importa el db.py
+with app.app_context():
+    from . import db
+    db.init_app(app)
 
 @app.route('/')
 def hello():
     return 'Que, tal!'
+
+#Icono de la pagina 
+@app.route('/favicon.ico')
+def favicon():
+    return send_file('static/flavicon.ico')
+
